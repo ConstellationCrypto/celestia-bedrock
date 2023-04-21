5477f0a12a135c1153dbb1823d9aec6a30d91663
Javed Khan <tuxcanfly@gmail.com>
2023-04-21 08:45:01 +0530

    da: use celestia as da
    build(deps): update go-da to v0.1.0
    
    build(deps): update go-da to v0.2.0; add ctx
    
    da: add cli flags for da config
    
    da: add get timeout to da client
    
    build(deps): bump local-celestia-devnet to v0.12.7

--- op-batcher/batcher/config.go
+++ op-batcher/batcher/config.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ethereum-optimism/optimism/op-batcher/compressor"
 	"github.com/ethereum-optimism/optimism/op-batcher/flags"
+	celestia "github.com/ethereum-optimism/optimism/op-celestia"
 	plasma "github.com/ethereum-optimism/optimism/op-plasma"
 	oplog "github.com/ethereum-optimism/optimism/op-service/log"
 	opmetrics "github.com/ethereum-optimism/optimism/op-service/metrics"
@@ -91,12 +92,13 @@ type CLIConfig struct {
 	// ActiveSequencerCheckDuration is the duration between checks to determine the active sequencer endpoint.
 	ActiveSequencerCheckDuration time.Duration
 
-	TxMgrConfig   txmgr.CLIConfig
-	LogConfig     oplog.CLIConfig
-	MetricsConfig opmetrics.CLIConfig
-	PprofConfig   oppprof.CLIConfig
-	RPC           oprpc.CLIConfig
-	PlasmaDA      plasma.CLIConfig
+	TxMgrConfig      txmgr.CLIConfig
+	LogConfig        oplog.CLIConfig
+	MetricsConfig    opmetrics.CLIConfig
+	PprofConfig      oppprof.CLIConfig
+	RPC              oprpc.CLIConfig
+	PlasmaDA         plasma.CLIConfig
+	DaConfig         celestia.CLIConfig
 }
 
 func (c *CLIConfig) Check() error {
@@ -148,6 +150,9 @@ func (c *CLIConfig) Check() error {
 	if err := c.RPC.Check(); err != nil {
 		return err
 	}
+	if err := c.DaConfig.Check(); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -180,5 +185,6 @@ func NewConfig(ctx *cli.Context) *CLIConfig {
 		PprofConfig:                  oppprof.ReadCLIConfig(ctx),
 		RPC:                          oprpc.ReadCLIConfig(ctx),
 		PlasmaDA:                     plasma.ReadCLIConfig(ctx),
+		DaConfig:                     celestia.ReadCLIConfig(ctx),
 	}
 }
