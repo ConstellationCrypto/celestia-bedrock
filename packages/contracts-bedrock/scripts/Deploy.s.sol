84613391cb5332af6ec64676b7950a80bfa0b607
Ino Murko <ino.murko.github@protonmail.com>
2024-05-28 15:55:08 +0000

    adapt to building new images

--- packages/contracts-bedrock/scripts/Deploy.s.sol
+++ packages/contracts-bedrock/scripts/Deploy.s.sol
@@ -425,7 +425,8 @@ contract Deploy is Deployer {
         address[] memory owners = new address[](0);
         addr_ = deploySafe(_name, owners, 1, true);
     }
-
+    // added on top of 1.7.6 release
+    // https://github.com/ethereum-optimism/optimism/pull/10660/files
     function deploySafe(
         string memory _name,
         address[] memory _owners,
@@ -435,7 +436,8 @@ contract Deploy is Deployer {
         public
         returns (address addr_)
     {
-        console.log("Deploying safe: %s ", _name);
+        bytes32 salt = keccak256(abi.encode(_name, _implSalt()));
+        console.log("Deploying safe: %s with salt %s", _name, vm.toString(salt));
         (SafeProxyFactory safeProxyFactory, Safe safeSingleton) = _getSafeFactory();
 
         address[] memory expandedOwners = new address[](_owners.length + 1);
@@ -452,11 +454,7 @@ contract Deploy is Deployer {
         bytes memory initData = abi.encodeCall(
             Safe.setup, (_owners, _threshold, address(0), hex"", address(0), address(0), 0, payable(address(0)))
         );
-        addr_ = address(
-            safeProxyFactory.createProxyWithNonce(
-                address(safeSingleton), initData, uint256(keccak256(abi.encode(_name)))
-            )
-        );
+        addr_ = address(safeProxyFactory.createProxyWithNonce(address(safeSingleton), initData, uint256(salt)));
 
         save(_name, addr_);
         console.log("New safe: %s deployed at %s\n    Note that this safe is owned by the deployer key", _name, addr_);
--- packages/contracts-bedrock/scripts/Executables.sol
+++ packages/contracts-bedrock/scripts/Executables.sol
@@ -23,7 +23,7 @@ library Executables {
         string[] memory commands = new string[](3);
         commands[0] = bash;
         commands[1] = "-c";
-        commands[2] = "cast abi-encode 'f(string)' $(git rev-parse HEAD)";
+        commands[2] = "cast abi-encode 'f(string)' v1.7.6";
         return abi.decode(vm.ffi(commands), (string));
     }
 }
