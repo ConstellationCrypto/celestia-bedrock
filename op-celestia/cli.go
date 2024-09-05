ae1e460fa7b11869b0a36f7ea20fea19ec3c4417
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2024-09-05 15:45:44 +0200

    op celestaia integration, deployment

---
+++ op-celestia/cli.go
@@ -0,0 +1,71 @@
+package celestia
+
+import (
+	"time"
+
+	"github.com/urfave/cli/v2"
+
+	opservice "github.com/ethereum-optimism/optimism/op-service"
+)
+
+func CLIFlags(envPrefix string) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:    "da-rpc",
+			Usage:   "dial address of data availability grpc client",
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "DA_RPC"),
+		},
+		&cli.StringFlag{
+			Name:    "namespace-id",
+			Usage:   "Namespace ID for DA node",
+			Value:   "00000000000000000000",
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "NAMESPACE_ID"),
+		},
+		&cli.StringFlag{
+			Name:    "auth-token",
+			Usage:   "Authentication Token for DA node",
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "AUTH_TOKEN"),
+		},
+		&cli.StringFlag{
+			Name:    "s3-bucket",
+			Usage:   "S3 Bucket for DA layer",
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "S3_BUCKET"),
+		},
+		&cli.StringFlag{
+			Name:    "s3-region",
+			Usage:   "S3 Region for DA layer",
+			Value:   "us-west-2",
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "S3_REGION"),
+		},
+		&cli.DurationFlag{
+			Name:    "celestia-timeout",
+			Usage:   "timeout for celestia requests",
+			Value:   time.Minute,
+			EnvVars: opservice.PrefixEnvVar(envPrefix, "CELESTIA_TIMEOUT"),
+		},
+	}
+}
+
+type CLIConfig struct {
+	DaRpc     string
+	Namespace string
+	AuthToken string
+	S3Bucket  string
+	S3Region  string
+	Timeout   time.Duration
+}
+
+func (c CLIConfig) Check() error {
+	return nil
+}
+
+func ReadCLIConfig(ctx *cli.Context) CLIConfig {
+	return CLIConfig{
+		DaRpc:     ctx.String("da-rpc"),
+		Namespace: ctx.String("namespace-id"),
+		AuthToken: ctx.String("auth-token"),
+		S3Bucket:  ctx.String("s3-bucket"),
+		S3Region:  ctx.String("s3-region"),
+		Timeout:   ctx.Duration("celestia-timeout"),
+	}
+}
