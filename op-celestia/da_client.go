c68051de9ec8522c67101e73195c54adcfa2c316
Ino Murko <ino.murko.github@protonmail.com>
2025-04-28 20:15:58 +0000

    tiny cleanup

--- op-celestia/da_client.go
+++ op-celestia/da_client.go
@@ -36,10 +36,10 @@ func NewDAClient(rpc, token, namespace, fallbackMode string, gasPrice float64, s
 	//CALDERA does not tolarate 58 size strings for namespace
 	//we have to fix this here
 	//and then again adjust in calldata_source downloadS3Data and driver.go uploadS3Data to trim
-	log.Warn("Checking namespace for backwards compatibility.", "len", len(namespace))
+	log.Warn("celestia: Checking namespace for backwards compatibility.", "len", len(namespace))
 	if len(namespace) != 58 {
 		namespace = "00000000000000000000000000000000000000" + namespace
-		log.Warn("Namespace has been adjusted.", "namespace", namespace)
+		log.Warn("celestia: Namespace has been adjusted.", "namespace", namespace)
 	}
 
 	ns, err := hex.DecodeString(namespace)
