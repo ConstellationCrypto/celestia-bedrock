d920b5141f85e17f9f874f822725e2e644031a03
Ino Murko <ino.murko.github@protonmail.com>
2025-11-06 17:31:43 +0000

    adjust namespace

--- op-celestia/da_client.go
+++ op-celestia/da_client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -59,11 +60,13 @@ func NewDAClient(rpc, token, namespace, fallbackMode string, gasPrice float64, s
 	//we have to fix this here
 	//and then again adjust in calldata_source downloadS3Data and driver.go uploadS3Data to trim
 	log.Warn("celestia: Checking namespace for backwards compatibility.", "len", len(namespace))
-	if len(namespace) != 58 {
-		namespace = "00000000000000000000000000000000000000" + namespace
-		log.Warn("celestia: Namespace has been adjusted.", "namespace", namespace)
+	// Generate namespace with exactly 58 characters
+	// we used to trim down the celestia namespace we're now migrating it to the celestia standard
+	requiredZeros := 58 - len(namespace)
+	if requiredZeros < 0 {
+		requiredZeros = 0
 	}
-	ns, err := hex.DecodeString(namespace)
+	ns, err := hex.DecodeString(strings.Repeat("0", requiredZeros))
 	if err != nil {
 		return nil, err
 	}
